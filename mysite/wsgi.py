406a15bcbef03340cbbf957eaa4ceb58dfb3ea21
Harry Stock <harrystock@outlook.com>
2015-11-15 13:42:05 +0000

    15/11/15

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -5,10 +5,11 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
-"""
+
 
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+"""
